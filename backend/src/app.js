de986cef069171995e6fb8c55cf8e095ff71bdb1
Thamasha <tlr.voniish@gmail.com>
2025-08-22 12:04:26 +1200

    Updated backend folder structure and database configuration
    Created models, routes, controllers, and middleware folders under src
    
    Moved database logic to src/config and configured connection
    
    Added .env.example for environment variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,23 @@
-import express from 'express';
+import express from "express";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen(3000, () => {
-  console.log('Server is running on: http://localhost:3000');
+/*
+Register the routes
+Examples:
+    app.use("/api/users", usersRoutes);
+    app.use("/api/journals", journalsRoutes);
+*/
+
+// Connect to MongoDB
+console.log("Connecting to MongoDB: ", process.env.MONGO_URI);
+  connectDB();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server is running on: http://localhost:" + PORT);
 });
