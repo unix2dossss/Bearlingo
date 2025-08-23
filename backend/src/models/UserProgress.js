2f12446a03bb3396995d5c98b0385660c90babe8
Thamasha <tlr.voniish@gmail.com>
2025-08-23 22:05:00 +1200

    Implemented UserProgress schema to track level progress in user's chosen modules

---
+++ backend/src/models/UserProgress.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const userProgressSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }, 
+    module: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Module',
+        required: true
+    }, 
+    levelProgress: {
+        level:{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Level',
+            required: true
+        },
+        completedSubtasks: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Subtask',
+            required: true
+        }],
+        badgeEarned: { 
+            type: Boolean, default: false 
+        },
+        progress: {
+            type: Number,
+        }
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model("UserProgress", userProgressSchema);
