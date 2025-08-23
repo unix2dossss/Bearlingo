75ac657dafa2b6b282b833c585c3033b90fd0cf4
Thamasha <tlr.voniish@gmail.com>
2025-08-23 22:55:57 +1200

    Created the journalEntry schema

---
+++ backend/src/models/JournalEntry.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const journalEntrySchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    timestamp: true
+});
+
+export default mongoose.model("JournalEntry", journalEntrySchema);
