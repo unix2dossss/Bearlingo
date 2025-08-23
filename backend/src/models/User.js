eb8ecd1ccebd182c87f8f977cc5bb5dbfae7f765
Thamasha <tlr.voniish@gmail.com>
2025-08-23 22:39:42 +1200

    Added lastActiveProgress field to User schema to track user's last activity

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -14,35 +14,42 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
-    select: false  // Exclude password from queries by default
+    select: false // Exclude password from queries by default
   },
-    xp: {
-        type: Number,
-        default: 0
+  xp: {
+    type: Number,
+    default: 0
+  },
+  streak: {
+    current: {
+      type: Number,
+      default: 0
     },
-    streak: {
-        current: {
-            type: Number,
-            default: 0
-        },
-        longest: {
-            type: Number,
-            default: 0
-        },
-        lastActive: {
-            type: Date
-        }
+    longest: {
+      type: Number,
+      default: 0
     },
-    progress: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "UserProgress"
-    }],
-    journalEntries: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "JournalEntry"
-    }],
+    lastActive: {
+      type: Date
+    }
+  },
+  lastActiveProgress: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "UserProgress"
+  },
+  progress: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "UserProgress"
+    }
+  ],
+  journalEntries: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "JournalEntry"
+    }
+  ],
   timestamp: true
 });
 
 export default mongoose.model("User", userSchema);
-
