0d29064b7be56cc2c3b59c931e5e3438d1d20941
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-08-23 20:43:17 +1200

    Added schema for each module (e.g. CV module) and used SchemaTypes to reference the schema Level

---
+++ backend/src/schemas/Module.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const { Schema, SchemaTypes, model } = mongoose;
+
+const moduleSchema = new Schema({
+    name: { type: String, required: true },
+    description: String,
+    levels: {
+        type: SchemaTypes.ObjectId,
+        ref: 'Level',
+        required: true,
+    },
+    timestamps: true
+});
+
+
+const Module = model('Module', moduleSchema);
+export default Module;
