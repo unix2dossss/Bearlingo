9caeb2785f412371ef8ecdbb65f8aa18bf40a8c9
Thamasha <tlr.voniish@gmail.com>
2025-08-23 21:31:03 +1200

    Created the User schema

---
+++ backend/src/models/User.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false  // Exclude password from queries by default
+  },
+    xp: {
+        type: Number,
+        default: 0
+    },
+    streak: {
+        current: {
+            type: Number,
+            default: 0
+        },
+        longest: {
+            type: Number,
+            default: 0
+        },
+        lastActive: {
+            type: Date
+        }
+    },
+    progress: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "UserProgress"
+    }],
+    journalEntries: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "JournalEntry"
+    }],
+  timestamp: true
+});
+
+export default mongoose.model("User", userSchema);
+
