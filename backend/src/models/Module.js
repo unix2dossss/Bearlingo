f08b235c1ecd838f691de8ba4d9f616b2d04d295
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-08-23 21:23:04 +1200

    Added a default empty array for levels field in Module schema for testing purposes

--- backend/src/models/Module.js
+++ backend/src/models/Module.js
@@ -5,13 +5,16 @@ const { Schema, SchemaTypes, model } = mongoose;
 const moduleSchema = new Schema({
     name: { type: String, required: true },
     description: String,
-    levels: [
-        {
-            type: SchemaTypes.ObjectId,
-            ref: 'Level',
-            required: true,
-        }
-    ],
+    levels: {
+        type: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Level",
+                required: true,
+            }
+        ],
+        default: []   // default is empty array for testing purposes
+    },
     timestamps: true
 });
 
