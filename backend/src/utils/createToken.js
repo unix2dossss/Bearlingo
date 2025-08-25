7de4a0d367ccee6ad46d3f492bc7ef771cedc677
Thamasha <tlr.voniish@gmail.com>
2025-08-25 16:58:39 +1200

    Created utils folder and added createToken.js helper

---
+++ backend/src/utils/createToken.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  // Set JWT as an HTTP-Only Cookie
+  res.cookie("jwt", token, {
+    httpOnly: true, // Accessible only by the web server and prevent XSS attacks
+    secure: process.env.NODE_ENV !== "development", // Cookie is only sent over HTTPS (not HTTP) if in production
+    sameSite: "strict", // Prevent CSRF attacks
+    maxAge: 30 * 24 * 60 * 60 * 1000, // Expire in 30 days 
+  });
+
+  return token;
+};
+
+export default generateToken;
