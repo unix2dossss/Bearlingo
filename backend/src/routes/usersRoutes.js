8d01602f295014f2a33c6548310c474f6a4f5afa
Thamasha <tlr.voniish@gmail.com>
2025-08-31 12:24:41 +1200

    Applied validateRegisterInputs and handleValidationErrors middleware to the /register route in usersRoutes.js. Fixes #4

--- backend/src/routes/usersRoutes.js
+++ backend/src/routes/usersRoutes.js
@@ -5,10 +5,11 @@ import * as usersController from "../controllers/usersController.js";
 
 //middlewares
 import { authenticate } from "../middlewares/authMiddleware.js";
+import { validateRegisterInputs, handleValidationErrors } from "../middlewares/inputValidators.js";
 
 const router = express.Router();
 
-router.post("/register", usersController.registerUser);
+router.post("/register", validateRegisterInputs, handleValidationErrors, registerUser);
 router.post("/login", usersController.loginUserToken);
 router.get("/add", usersController.addUser);
 router.get("/:id", usersController.getUserInfo);
