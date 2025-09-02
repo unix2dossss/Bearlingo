b1307c49a387d7fa9397d3d6cf216567dc23dfd2
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-02 13:08:38 +1200

    Adding paths to App.jsx file

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 const App = () => {
   return (
     <div>
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   )
 }
