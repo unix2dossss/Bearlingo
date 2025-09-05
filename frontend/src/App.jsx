9efec72b1f971db84022e92c732060aa952d53b9
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-06 11:45:40 +1200

    Changed front end so that it is able to show all paths

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
