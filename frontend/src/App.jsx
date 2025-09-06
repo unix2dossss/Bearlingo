bfc9be6627f07d7a66060d082de8c77c8e3006d3
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-06 12:06:04 +1200

    Added taost notifcation on button for testing purposes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom';
+import toast from "react-hot-toast";
 
 const App = () => {
   return (
     <div>
+      <button onClick={() => toast.success("yay toast works")}>Click me</button>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register />} />
