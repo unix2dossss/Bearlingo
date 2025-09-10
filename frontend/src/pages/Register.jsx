3f531f9ecae4e95db3cf097165d555634d0b5e62
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-10 14:31:48 +1200

    Made the sign up card smaller

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -142,122 +142,126 @@ const Register = () => {
                             Back to Home
                         </Link>
 
-                        <div className="card bg-base-100">
-                            <div className="card-body">
-                                <h2 className="card-title text-2xl mb flex justify-center">Sign Up</h2>
-                                <form onSubmit={handleSubmit}>
-                                    <div className="flex gap-4">
-                                        <div className="form-control mb-4 w-full">
+                        <div className="flex items-center justify-center">
+
+
+                            <div className="card bg-base-100 border border-red-600 w-[900px] justify-center">
+                                <div className="card-body">
+                                    <h2 className="card-title text-2xl mb flex justify-center">Sign Up</h2>
+                                    <form onSubmit={handleSubmit}>
+                                        <div className="flex gap-4">
+                                            <div className="form-control mb-4 w-full">
+                                                <label className="label">
+                                                    <span className="label-text">First Name  <span className="label-text text-red-600">*</span></span>
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    placeholder="First Name"
+                                                    className="input input-bordered"
+                                                    value={firstName}
+                                                    onChange={(e) => setFirstName(e.target.value)}
+                                                ></input>
+                                            </div>
+                                            <div className="form-control mb-4 w-full">
+                                                <label className="label">
+                                                    <span className="label-text">Last Name <span className="label-text text-red-600">*</span></span>
+                                                </label>
+                                                <input
+                                                    type="text"
+                                                    placeholder="Last Name"
+                                                    className="input input-bordered"
+                                                    value={lastName}
+                                                    onChange={(e) => setLastName(e.target.value)}
+                                                ></input>
+                                            </div>
+                                        </div>
+                                        <div className="form-control mb-4">
                                             <label className="label">
-                                                <span className="label-text">First Name  <span className="label-text text-red-600">*</span></span>
+                                                <span className="label-text">Username <span className="label-text text-red-600">*</span></span>
                                             </label>
                                             <input
                                                 type="text"
-                                                placeholder="First Name"
+                                                placeholder="Username"
                                                 className="input input-bordered"
-                                                value={firstName}
-                                                onChange={(e) => setFirstName(e.target.value)}
+                                                value={username}
+                                                onChange={(e) => setUsername(e.target.value)}
                                             ></input>
                                         </div>
-                                        <div className="form-control mb-4  w-full">
+                                        <div className="form-control mb-4">
                                             <label className="label">
-                                                <span className="label-text">Last Name <span className="label-text text-red-600">*</span></span>
+                                                <span className="label-text">LinkedIn (Optional)</span>
                                             </label>
                                             <input
                                                 type="text"
-                                                placeholder="Last Name"
+                                                placeholder="LinkedIn"
                                                 className="input input-bordered"
-                                                value={lastName}
-                                                onChange={(e) => setLastName(e.target.value)}
+                                                value={linkedIn}
+                                                onChange={(e) => setLinkedIn(e.target.value)}
                                             ></input>
                                         </div>
-                                    </div>
-                                    <div className="form-control mb-4">
-                                        <label className="label">
-                                            <span className="label-text">Username <span className="label-text text-red-600">*</span></span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            placeholder="Username"
-                                            className="input input-bordered"
-                                            value={username}
-                                            onChange={(e) => setUsername(e.target.value)}
-                                        ></input>
-                                    </div>
-                                    <div className="form-control mb-4">
-                                        <label className="label">
-                                            <span className="label-text">LinkedIn (Optional)</span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            placeholder="LinkedIn"
-                                            className="input input-bordered"
-                                            value={linkedIn}
-                                            onChange={(e) => setLinkedIn(e.target.value)}
-                                        ></input>
-                                    </div>
-                                    <div className="form-control mb-4">
-                                        <label className="label">
-                                            <span className="label-text">Email <span className="label-text text-red-600">*</span></span>
-                                        </label>
-                                        <input
-                                            type="text"
-                                            placeholder="Email"
-                                            className="input input-bordered"
-                                            value={email}
-                                            onChange={(e) => setEmail(e.target.value)}
-                                        ></input>
-                                    </div>
-                                    <div className="form-control mb-4">
-                                        <label className="label">
-                                            <span className="label-text">Password <span className="label-text text-red-600">*</span></span>
-                                        </label>
-                                        <input
-                                            type="password"
-                                            placeholder="Password"
-                                            className="input validator input-bordered"
-                                            minlength="8"
-                                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-                                            onFocus={() => setFocused(true)}   // show hint on focus
-                                            onBlur={() => setFocused(false)}   // hide hint on blur
-                                            value={password}
-                                            onChange={(e) => setPassword(e.target.value)}
-                                        ></input>
-                                        {focused && !passwordPattern.test(password) && (<p className="validator-hint text-sm text-red-600">
-                                            Must be more than 8 characters, including
-                                            <br />At least one number. At least one lowercase letter. At least one uppercase letter
-                                        </p>
-                                        )}
-                                    </div>
-                                    <div className="form-control mb-4">
-                                        <label className="label">
-                                            <span className="label-text">Confirm Password <span className="label-text text-red-600">*</span></span>
-                                        </label>
-                                        <input
-                                            type="password"
-                                            placeholder="Confirm Password"
-                                            className="input validator input-bordered"
-                                            minlength="8"
-                                            pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
-                                            title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
-                                            onFocus={() => setFocused2(true)}   // show hint on focus
-                                            onBlur={() => setFocused2(false)}   // hide hint on blur
-                                            value={confirmPassword}
-                                            onChange={(e) => setConfirmPassword(e.target.value)}
-                                        ></input>
-                                        {focused2 && !passwordPattern.test(confirmPassword) && (<p className="validator-hint text-sm text-red-600">
-                                            Must be more than 8 characters, including
-                                            <br />At least one number. At least one lowercase letter. At least one uppercase letter
-                                        </p>
-                                        )}
-                                    </div>
-                                    <div className="card-actions justify-center mb-2 h-20">
-                                        <button type="submit" className="btn btn-primary w-40 h-18" disable={loading}>
-                                            {loading ? "Registering ..." : "Register"}
-                                        </button>
-                                    </div>
-                                </form>
+                                        <div className="form-control mb-4">
+                                            <label className="label">
+                                                <span className="label-text">Email <span className="label-text text-red-600">*</span></span>
+                                            </label>
+                                            <input
+                                                type="text"
+                                                placeholder="Email"
+                                                className="input input-bordered"
+                                                value={email}
+                                                onChange={(e) => setEmail(e.target.value)}
+                                            ></input>
+                                        </div>
+                                        <div className="form-control mb-4">
+                                            <label className="label">
+                                                <span className="label-text">Password <span className="label-text text-red-600">*</span></span>
+                                            </label>
+                                            <input
+                                                type="password"
+                                                placeholder="Password"
+                                                className="input validator input-bordered"
+                                                minlength="8"
+                                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                                                onFocus={() => setFocused(true)}   // show hint on focus
+                                                onBlur={() => setFocused(false)}   // hide hint on blur
+                                                value={password}
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            ></input>
+                                            {focused && !passwordPattern.test(password) && (<p className="validator-hint text-sm text-red-600">
+                                                Must be more than 8 characters, including
+                                                <br />At least one number. At least one lowercase letter. At least one uppercase letter
+                                            </p>
+                                            )}
+                                        </div>
+                                        <div className="form-control mb-4">
+                                            <label className="label">
+                                                <span className="label-text">Confirm Password <span className="label-text text-red-600">*</span></span>
+                                            </label>
+                                            <input
+                                                type="password"
+                                                placeholder="Confirm Password"
+                                                className="input validator input-bordered"
+                                                minlength="8"
+                                                pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+                                                title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+                                                onFocus={() => setFocused2(true)}   // show hint on focus
+                                                onBlur={() => setFocused2(false)}   // hide hint on blur
+                                                value={confirmPassword}
+                                                onChange={(e) => setConfirmPassword(e.target.value)}
+                                            ></input>
+                                            {focused2 && !passwordPattern.test(confirmPassword) && (<p className="validator-hint text-sm text-red-600">
+                                                Must be more than 8 characters, including
+                                                <br />At least one number. At least one lowercase letter. At least one uppercase letter
+                                            </p>
+                                            )}
+                                        </div>
+                                        <div className="card-actions justify-center mb-2 h-20">
+                                            <button type="submit" className="btn btn-primary w-40 h-18" disable={loading}>
+                                                {loading ? "Registering ..." : "Register"}
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
