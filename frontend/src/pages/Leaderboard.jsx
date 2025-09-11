25dc32246081704d102707d4ed662c85603ea93e
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-11 15:16:31 +1200

    Added badges for users who are in 1st, 2nd or 3rd place using React JSX realising that using ternary operations or logical AND statements can be used for JSX expressions

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -5,6 +5,9 @@ import TopNavbar from "../components/TopNavbar";
 import linkedInIcon from "../assets/linkedin.png";
 import starIcon from "../assets/star.png";
 import bearIcon from "../assets/Bear.svg";
+import goldMedal from "../assets/gold.png"
+import silverMedal from "../assets/silver.png";
+import bronzeMedal from "../assets/bronze.png"
 
 
 const Leaderboard = () => {
@@ -35,6 +38,9 @@ const Leaderboard = () => {
         <div className="bg-blue-200 min-h-screen">
 
             <TopNavbar />
+            <div className="flex pr-2 mt-2 justify-end">
+                <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+            </div>
 
             <br />
             <br />
@@ -44,38 +50,60 @@ const Leaderboard = () => {
                     <h1 className=" text-4xl font-bold text-center flex justify-center font-sans">Leaderboard</h1>
                 </div>
             </div>
+            <br />
+            <br />
 
-            <input type="checkbox" value="synthwave" className="toggle theme-controller" />
+
             <div className="flex justify-center">
-                <ul className="list bg-base-100 rounded-box shadow-md justify-center  w-[800px] h-[500px] overflow-y-auto">
+                <ul className="list bg-base-100 rounded-box shadow-md justify-center  w-[800px] h-[600px] overflow-y-auto">
                     {leaderboardData.map((user, index) => (
-                        <li className="list-row flex items-center p-2 border-b" key={index}>
+                        <li className="list-row flex items-center p-2 border-b h-[100px]" key={index}>
 
                             {/* Position on leaderboard */}
-                            <div className=" w-[50px] text-4xl font-thin opacity-30 tabular-nums">{index < 3 ? ":)" : index + 1}</div>
+                            {index === 0 && (
+                                < div className="w-[100px] text-4xl font-thin opacity-100 tabular-nums ml-3"><img height="35" width="35" src={goldMedal} alt="Gold medal"></img></div>
+                            )}
+
+                            {index === 1 && (
+                                < div className="w-[100px] text-4xl font-thin opacity-100 tabular-nums ml-3"><img height="35" width="35" src={silverMedal} alt="Silver medal"></img></div>
+                            )}
+
+                            {index === 2 && (
+                                < div className="w-[100px] text-4xl font-thin opacity-100 tabular-nums ml-3"><img height="35" width="35" src={bronzeMedal} alt="Bronze medal"></img></div>
+                            )}
+
+                            {![0, 1, 2].includes(index) && (
+                                < div className="w-[100px] text-4xl font-thin opacity-30 tabular-nums ml-3">{index + 1}</div>
+                            )}
+
+
+
 
                             {/* Profile Image */}
-                            <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
+                            <div className="w-[100px]"><img className=" size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
 
                             {/* Username */}
-                            <div className="w-[50px]">{user.username}</div>
+                            < div className="w-[250px] flex justify-center" > {user.username}</div>
 
                             {/* LinkedIn icon*/}
-                            <button className="btn btn-square btn-ghost">
-                                <img height="35" width="35" src={linkedInIcon} alt="LinkedIn Icon"></img>
-                            </button>
+                            <div className=" w-[190px]">
+                                <button className=" btn btn-square btn-ghost">
+                                    <img height="35" width="35" src={linkedInIcon} alt="LinkedIn Icon"></img>
+                                </button>
+                            </div>
 
                             {/* XP */}
-                            <div className="flex gap-10">
+                            < div className=" flex gap-2 justify-center align-center" >
                                 <img height="35" width="35" src={starIcon} alt="Star Icon"></img>
-                                <div>{user.xp}</div>
+                                <div className="flex justify-center align-center">{user.xp}</div>
                             </div>
                         </li >
 
-                    ))}
+                    ))
+                    }
                 </ul >
             </div >
-        </div>
+        </div >
 
 
 
