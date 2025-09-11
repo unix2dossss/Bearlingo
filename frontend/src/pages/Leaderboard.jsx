b76d9a534c1aedd56794037e372e9681cbd995bc
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-11 14:33:14 +1200

    Changed background colour, added a header and fixing layout of the items for each row on the leaderboard

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -32,28 +32,40 @@ const Leaderboard = () => {
 
 
     return (
-        <>
+        <div className="bg-blue-200 min-h-screen">
 
             <TopNavbar />
+
             <br />
             <br />
+            <div className="flex justify-center">
+                <div className="bg-yellow-300 text-white px-8 py-4 rounded-full shadow-lg w-[300px] border-4 border-white">
+
+                    <h1 className=" text-4xl font-bold text-center flex justify-center font-sans">Leaderboard</h1>
+                </div>
+            </div>
 
             <input type="checkbox" value="synthwave" className="toggle theme-controller" />
             <div className="flex justify-center">
                 <ul className="list bg-base-100 rounded-box shadow-md justify-center  w-[800px] h-[500px] overflow-y-auto">
                     {leaderboardData.map((user, index) => (
-                        <li className="list-row flex items-center justify-between gap-4 p-2 border-b" key={index}>
+                        <li className="list-row flex items-center p-2 border-b" key={index}>
 
-                            <div className="text-4xl font-thin opacity-30 tabular-nums">{index < 3 ? ":)" : index + 1}</div>
+                            {/* Position on leaderboard */}
+                            <div className=" w-[50px] text-4xl font-thin opacity-30 tabular-nums">{index < 3 ? ":)" : index + 1}</div>
+
+                            {/* Profile Image */}
                             <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
 
-                            <div>
-                                <div>{user.username}</div>
-                            </div>
+                            {/* Username */}
+                            <div className="w-[50px]">{user.username}</div>
+
+                            {/* LinkedIn icon*/}
                             <button className="btn btn-square btn-ghost">
                                 <img height="35" width="35" src={linkedInIcon} alt="LinkedIn Icon"></img>
                             </button>
 
+                            {/* XP */}
                             <div className="flex gap-10">
                                 <img height="35" width="35" src={starIcon} alt="Star Icon"></img>
                                 <div>{user.xp}</div>
@@ -63,10 +75,11 @@ const Leaderboard = () => {
                     ))}
                 </ul >
             </div >
+        </div>
 
 
 
-        </>
+
     )
 }
 
