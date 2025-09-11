dda0a17a7e91d3432d2b41dde20e5d452e1e71e4
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-11 12:03:14 +1200

    Added scrollable leaderbaord by giving it a fixed height and using  overflow-y-auto

--- frontend/src/pages/Leaderboard.jsx
+++ frontend/src/pages/Leaderboard.jsx
@@ -3,6 +3,8 @@ import toast from 'react-hot-toast';
 import api from "../lib/axios";
 import TopNavbar from "../components/TopNavbar";
 import linkedInIcon from "../assets/linkedin.png";
+import starIcon from "../assets/star.png";
+import bearIcon from "../assets/Bear.svg";
 
 
 const Leaderboard = () => {
@@ -38,27 +40,28 @@ const Leaderboard = () => {
 
             <input type="checkbox" value="synthwave" className="toggle theme-controller" />
             <div className="flex justify-center">
-                <ul className="list bg-base-100 rounded-box shadow-md justify-center w-[800px]">
+                <ul className="list bg-base-100 rounded-box shadow-md justify-center  w-[800px] h-[500px] overflow-y-auto">
                     {leaderboardData.map((user, index) => (
                         <li className="list-row flex items-center justify-between gap-4 p-2 border-b" key={index}>
 
-                            <div className="text-4xl font-thin opacity-30 tabular-nums">{index < 3 ? index + 1 : ""}</div>
+                            <div className="text-4xl font-thin opacity-30 tabular-nums">{index < 3 ? ":)" : index + 1}</div>
                             <div><img className="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
 
                             <div>
                                 <div>{user.username}</div>
                             </div>
                             <button className="btn btn-square btn-ghost">
-                                <img src={linkedInIcon} alt="LinkedIn Icon"></img>
+                                <img height="35" width="35" src={linkedInIcon} alt="LinkedIn Icon"></img>
                             </button>
 
-                            <div>
-
+                            <div className="flex gap-10">
+                                <img height="35" width="35" src={starIcon} alt="Star Icon"></img>
+                                <div>{user.xp}</div>
                             </div>
-                        </li>
+                        </li >
 
                     ))}
-                </ul>
+                </ul >
             </div >
 
 
