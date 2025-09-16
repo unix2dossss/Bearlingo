3e5f1b6cda07aaf5b5e8937cdef5be91bab538ce
daisy-of-the-valley <deizat1111@gmail.com>
2025-09-16 19:40:25 +1200

    Integrate popup and file logic into the Journal

--- frontend/src/pages/Journal.jsx
+++ frontend/src/pages/Journal.jsx
@@ -65,41 +65,68 @@ const Journal = () => {
                                 ))}
                             </div>
                         </div>
-                        
-
-                        {/* Top nav bar */}
-                        {/* <div className="bg-slate-500/30 shadow-sm h-[50px] p-3 flex items-center justify-center">
-                            <a className="text-white font-bold">Its Goal Setting Time User!</a>
-                        </div> */}
-
-                        {/* Folders */}
-                        {/* <div className="flex flex-col items-end gap-2 ml-[750px] mt-[10px] mr-[10px] w-[100px] h-[318px] rounded-lg bg-rose-300 ">
-                            {folders.map((folder, index) => (
-                                <div
-                                    key={index}
-                                    className="w-16 h-16 flex flex-col items-center m-4 cursor-pointer"
-                                >
-                                    <button onClick={() => setOpenFolder(folder)}>
-                                        {console.log("folder: ", folder)}
-                                        <img
-                                            src={FolderImage}
-                                            alt="Folder"
-                                            className="w-12 h-12 flex justify-center"
-                                        />
-                                    </button>
-                                    <span className="text-white text-sm mt-1">{folder.name}</span>
-                                </div>
-                            ))}
-                        </div> */}
-
-                        {/* Files of a folder */}
                     </div>
-
-                    {/*<div className="w-16 h-10 bg-gray-700 mx-auto rounded-b-full"></div>*/}
-                    {/* <div className="w-56 h-[70px] bg-gray-700 mx-auto"></div> */}
                 </div >
-            </div >
 
+                {openFolder && (
+                    <>
+                        <div className="fixed inset-0 flex justify-center items-center z-50 bg-black/30">
+                            <div className="relative bg-base-100 mockup-window max-w-6xl w-[90%] h-[90%]"> {/*} border-base-300 */}
+                                <div className="border border-stone-400">
+
+                                    <button
+                                        className="flex justify-center rounded-full absolute left-4 top-4 bg-red-600 btn-error w-[25px] h-[25px] "
+                                        onClick={() => setOpenFolder(null)}
+                                    >
+                                        X
+                                    </button>
+                                </div>
+                                <div className="flex flex-row h-full gap-0">
+                                    <div className="w-[190px] bg-purple-400 h-full p-[8px]">
+                                        <ul className="mt-4 flex flex-col gap-2">
+                                            {openFolder.files.map((file, i) => (
+                                                <button onClick={() => setOpenFile(file)}>
+                                                    {console.log("Opened file: ", file)}
+                                                    <li
+                                                        key={i}
+                                                        className="flex justify-center p-3 bg-purple-200 text-purple-800 hover:bg-gray-200 cursor-pointer"
+                                                    >
+                                                        {file}
+                                                    </li>
+                                                </button>
+
+                                            ))}
+                                            <li className="flex justify-center p-3 bg-purple-400 border border-white text-white hover:bg-white hover:text-purple-800 cursor-pointer">
+                                                <button onClick={() => ""} >
+                                                    + Add New File
+                                                </button>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                    {/* Showing a file */}
+                                    <div className="bg-purple-100 w-full">
+
+                                        <h2 className="flex justify-center text-3xl text-purple-400 mt-5">Goal Setting</h2>
+                                        <div className="flex flex-col ml-7">
+                                            <p className="text-purple-400 text-sm mt-4">What makes a goal SMART?</p>
+                                            <textarea placeholder="Type here" className="textarea textarea-neutral mt-2 w-96"></textarea>
+                                            <button className="ml-80 btn-neutral btn-outline border border-gray-600 h-5 w-16 text-sm">Submit</button>
+                                        </div>
+                                        <p className="text-purple-400 text-sm ml-7 mt-4">Top three SMART goals for the upcoming fortnight</p>
+                                        <textarea placeholder="Type here" className="textarea textarea-neutral ml-7 mt-2 w-96"></textarea>
+                                        {openFile && (
+                                            <>
+                                                {console.log("IN OPEN FILE!!!")}
+
+                                            </>
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                )}
+            </div >
         </>
     )
 }
