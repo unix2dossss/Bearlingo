3bad3cbba4e22002423e30cd8f13377d62e5f667
vcha-uni <vcha997@aucklanduni.ac.nz>
2025-09-20 21:06:04 +1200

    Added min and max values like the likert scale

--- backend/src/models/networkingReflection.js
+++ backend/src/models/networkingReflection.js
@@ -4,19 +4,19 @@ const NetworkingReflectionSchema = new mongoose.Schema(
     {
         question1: {
             question: "I connected with someone new at this event",
-            answer: { type: Number, required: true }
+            answer: { type: Number, required: true, min: 1, max: 5 }
         },
         question2: {
             question: "I learned something valuable at this event",
-            answer: { type: Number, required: true }
+            answer: { type: Number, required: true, min: 1, max: 5 }
         },
         question3: {
             question: "I feel more confident about networking after this event",
-            answer: { type: Number, required: true }
+            answer: { type: Number, required: true, min: 1, max: 5 }
         },
         question4: {
             question: "I have a clear next step to follow up with people I met",
-            answer: { type: Number, required: true }
+            answer: { type: Number, required: true, min: 1, max: 5 }
         }
     },
     {
