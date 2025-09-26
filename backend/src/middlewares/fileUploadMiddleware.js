692a805f7a5e313e2161ff2567c4a87cd607ea3a
Thamasha <tlr.voniish@gmail.com>
2025-09-27 00:18:41 +1200

    Created simple CVuploadBox component for PDF CV upload, added frontend logic, required endpoints and middleware

---
+++ backend/src/middlewares/fileUploadMiddleware.js
@@ -0,0 +1,13 @@
+import multer from "multer";
+
+// Middleware for PDF file upload validation
+export const upload = multer({
+  storage: multer.memoryStorage(),
+  limits: { fileSize: 5 * 1024 * 1024 }, // 5 MB
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== "application/pdf") {
+      return cb(new Error("Only PDF files allowed"), false);
+    }
+    cb(null, true);
+  }
+});
