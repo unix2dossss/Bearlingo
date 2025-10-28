85b935c40b50a036905681617437eaa9cf947231
daisy-of-the-valley <deizat1111@gmail.com>
2025-10-28 17:47:37 +1300

    Update navbar

--- frontend/src/components/SideNavbar.jsx
+++ frontend/src/components/SideNavbar.jsx
@@ -11,7 +11,7 @@ const SideNavbar = () => {
     { id: 2, label: "Interview", path: "/InterviewModule" },
     { id: 3, label: "Networking", path: "/NetworkingModule" },
     { id: 4, label: "Journal", path: "/Journal" },
-    { id: 5, label: "Journal2", path: '/Journal2' },
+    // { id: 5, label: "Journal2", path: '/Journal2' },
     { id: 6, label: "Leaderboard", path: "/Leaderboard" },
   ];
 
